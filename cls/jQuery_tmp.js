240228c7e80743d2f7f52f1fd3b18d57c6c879f1
cti1650 <cti1650@gmail.com>
2016-02-12 03:49:31 +0000

    useClass.js 編集

---
+++ cls/jQuery_tmp.js
@@ -0,0 +1,22 @@
+(function($){
+
+  var methods = {
+    init : function( options ) {
+      settings = $.extend(true, {
+          'default': 'default val'
+        }, options);
+    },
+  };
+
+  $.fn.tooltip = function( method ) {
+
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }
+
+  };
+})(jQuery)
